51502b38ce890ad7ba5262ed7ae5d4f8f97dc88f
August7337 <augustin.tarit@gmail.com>
2024-05-01 21:20:22 +0200

    Test CKEditor

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/test', function () {
+        return view('test');
+    })->name('test');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
